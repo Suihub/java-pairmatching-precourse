8f703690f4ff7b68e392ea88ce92fff0a5522550
Suihub <qkdcrt@gmail.com>
2023-12-13 22:15:47 +0900

    <feat(MatchController)> : "컨트롤러 작성 및 View 작성" - 프로그램을 중계할 컨트롤러 작성 및 View 작성

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,12 @@
 package pairmatching;
 
+import pairmatching.controller.MatchController;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        MatchController controller = new MatchController(new InputView(), new OutputView());
+        controller.run();
     }
 }
---
+++ src/main/java/pairmatching/controller/MatchController.java
@@ -0,0 +1,18 @@
+package pairmatching.controller;
+
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+public class MatchController {
+    private final InputView input;
+    private final OutputView output;
+
+    public MatchController(InputView input, OutputView output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public void run() {
+
+    }
+}
