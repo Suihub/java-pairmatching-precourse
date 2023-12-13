3ae7c9309b89e426f468b721cc3fe12beafc7c32
Suihub <qkdcrt@gmail.com>
2023-12-14 00:55:50 +0900

    <feat(Crews)> : "페어 매칭 반환 기능 구현" - 랜덤하게 섞은 이름을 토대로 크루 리스트를 생성하는 기능 구현 - 위 크루 리스트를 토대로 페어 객체 리스트를 생성하는 기능 구현 - 최종적으로 Pairs 객체를 생성해 반환하는 기능 구현

--- src/main/java/pairmatching/domain/Crews.java
+++ src/main/java/pairmatching/domain/Crews.java
@@ -2,7 +2,9 @@ package pairmatching.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Crews {
     private final List<String> crewNames;
@@ -13,7 +15,33 @@ public class Crews {
         this.level = level;
     }
 
+    public Pairs pairMatching() {
+        List<Crew> crews = convertCrews(shuffleNames());
+        return new Pairs(convertPairs(crews));
+    }
+
     private List<String> shuffleNames() {
         return Randoms.shuffle(crewNames);
+    }
+
+    private List<Crew> convertCrews(List<String> shuffleNames) {
+        return shuffleNames.stream()
+                .map(name -> new Crew(level, name))
+                .collect(Collectors.toList());
+    }
+
+    private List<Pair> convertPairs(List<Crew> crews) {
+        List<Pair> pairs = new ArrayList<>();
+
+        for (int i = 0; i < crews.size(); i+=2) {
+            if (crews.size() - i == 3) {
+                Pair pair = new Pair(crews.subList(i, i + 2));
+                pairs.add(pair);
+                break;
+            }
+            Pair pair = new Pair(crews.subList(i, i + 1));
+            pairs.add(pair);
+        }
+        return pairs;
     }
 }
