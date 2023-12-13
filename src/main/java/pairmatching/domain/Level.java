f3cefdc1d27ee6770b11c9f293000686c80c52f5
Suihub <qkdcrt@gmail.com>
2023-12-13 22:58:46 +0900

    <feat(Level)> : "열거형 Level 구현" - 기능 목록 수정 - Level 구현 - 레벨 입력값 검증 구현 - 입력값에 따라 이에 해당하는 열거형 반환 구현

---
+++ src/main/java/pairmatching/domain/Level.java
@@ -0,0 +1,40 @@
+package pairmatching.domain;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum Level {
+    LEVEL1("레벨1"),
+    LEVEL2("레벨2"),
+    LEVEL3("레벨3"),
+    LEVEL4("레벨4"),
+    LEVEL5("레벨5");
+
+    private final String name;
+
+    Level(String name) {
+        this.name = name;
+    }
+
+    public static Level levelSetting(String input) {
+        validate(input);
+
+        Optional<Level> result = Arrays.stream(Level.values())
+                .filter(level -> level.name == input)
+                .findAny();
+
+        return result.orElseThrow(() -> {
+            throw new IllegalArgumentException("[ERROR] 존재하지 않는 레벨입니다.");
+        });
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private static void validate(String input) {
+        if (input.equals("레벨3") || input.equals("레벨5")) {
+            throw new IllegalArgumentException("[ERROR] 레벨3, 레벨5는 페어 매칭이 불가능합니다.");
+        }
+    }
+}
